0d3c00a42e49b04d05c7d477eed0d5b84c13c899
UnLuxier <kstas5309@gmail.com>
2026-01-17 19:56:48 +0200

    Multiplayer ctn

--- Assets/scripts/MultiPlayer/MenuManagerScript.cs
+++ Assets/scripts/MultiPlayer/MenuManagerScript.cs
@@ -8,15 +8,45 @@ public class MenuManagerScript : MonoBehaviourPunCallbacks
     public InputField createInput;
     public InputField joinInput;
 
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
     public void CreateRoom()
     {
+        if (!PhotonNetwork.InLobby)
+        {
+            Debug.LogWarning("Not in lobby yet!");
+            return;
+        }
+
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 4;
+
         PhotonNetwork.CreateRoom(createInput.text, roomOptions);
     }
 
     public void JoinRoom()
     {
+        if (!PhotonNetwork.InLobby)
+        {
+            Debug.LogWarning("Not in lobby yet!");
+            return;
+        }
+
         PhotonNetwork.JoinRoom(joinInput.text);
     }
 
